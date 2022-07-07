7130d57cd0f94d406fdeb4eb12f1be6454d305be
Fernando <fernando.luiz92@hotmail.com>
2022-07-07 06:11:55 -0300

    Preparando a estrutura do projeto

--- workspace/ambiente-angular/src/app/app.component.ts
+++ workspace/ambiente-angular/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ambiente-angular';
+
+export class AppComponent implements OnInit {
+
+  count = 0;
+  nome = 'Fernando Luiz';
+  text = '';
+
+  pessoas = []
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    let interval = setInterval(()=> {
+      this.count++;
+      if(this.count === 10){
+        clearInterval(interval)
+      }
+    }, 1000)
+  }
+
+  private clicou(nome: string): void{
+    console.log('Clicou em mim', nome)
+  }
 }
